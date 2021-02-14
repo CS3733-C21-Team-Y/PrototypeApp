db560effb7ef8d4ee2e402643cc9fce92751fb5b
Evan Davis <evan4489@me.com>
2021-02-14 13:20:02 -0500

    Initial Commit w/ empty-project starter

---
+++ src/test/java/edu/wpi/yellowyetis/DefaultTest.java
@@ -0,0 +1,13 @@
+/*-------------------------*/
+/* DO NOT DELETE THIS TEST */
+/*-------------------------*/
+
+package edu.wpi.yellowyetis;
+
+import org.junit.jupiter.api.Test;
+
+public class DefaultTest {
+
+  @Test
+  public void test() {}
+}
