da46277f0281d45a3566f2b287661c261f23a644
ajcorey <alexandercorey662@yahoo.com>
2021-02-15 17:46:05 -0500

    added a highlight and delete feature for edges, still need to comb it with comments and work out edge cases and get it going with real data

--- src/main/java/edu/wpi/yellowyetis/nodeEdgeDispController.java
+++ src/main/java/edu/wpi/yellowyetis/nodeEdgeDispController.java
@@ -1,6 +1,5 @@
 package edu.wpi.yellowyetis;
 
-import java.awt.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -25,11 +24,13 @@ public class nodeEdgeDispController {
   @FXML private CheckBox addEdgecb;
 
   @FXML private Button deleteNode;
+  @FXML private Button deleteEdge;
 
   // variables for selecting points and locating edges
-  private boolean startEdgeFlag = false;
+  private boolean startEdgeFlag = true;
   private int sx, sy, ex, ey;
-  private Circle a = new Circle(0, 0, 0);
+  private Circle currentSelectedCircle = new Circle(0, 0, 0);
+  private Line currentSelectedLine = new Line(0, 0, 0, 0);
 
   @FXML private TextField newX;
   @FXML private TextField newY;
@@ -82,19 +83,24 @@ public class nodeEdgeDispController {
     addEdge.setOnAction(e -> createEdge(e));
 
     deleteNode.setOnAction(e -> removeNode(e));
+    deleteEdge.setOnAction(e -> removeEdge(e));
 
     // create node or edge when pane is clicked
     pane.setOnMouseClicked(
         e -> {
-          getCircle();
+          getPaneNode();
+
           if (addEdgecb.isSelected()) {
             unHighlightCircle();
+            unHighglightLine();
             createEdgecb(e);
           } else if (addNodecb.isSelected()) {
             unHighlightCircle();
+            unHighglightLine();
             createNodecb(e);
           } else {
             highlightCircle();
+            highlightLine();
           }
         });
 
@@ -110,8 +116,12 @@ public class nodeEdgeDispController {
         });
   }
 
+  private void removeEdge(ActionEvent e) {
+    pane.getChildren().remove(currentSelectedLine);
+  }
+
   private void removeNode(ActionEvent e) {
-    pane.getChildren().remove(a);
+    pane.getChildren().remove(currentSelectedCircle);
 
     // adding the node and refreshing the scene
     Stage stage = (Stage) deleteNode.getScene().getWindow();
@@ -121,25 +131,43 @@ public class nodeEdgeDispController {
 
   private void highlightCircle() {
     // highlights the selected circle which is the startpoint of the edge
-    a.setStrokeWidth(2);
-    a.setStroke(Paint.valueOf("BLUE"));
+    currentSelectedCircle.setStrokeWidth(2);
+    currentSelectedCircle.setStroke(Paint.valueOf("BLUE"));
   }
 
   private void unHighlightCircle() {
     // removes highlight on circle
-    a.setStrokeWidth(0);
+    currentSelectedCircle.setStrokeWidth(0);
   }
 
-  private void getCircle() {
+  private void highlightLine() {
+    currentSelectedLine.setStrokeWidth(5);
+    currentSelectedLine.setStroke(Paint.valueOf("BLUE"));
+  }
+
+  private void unHighglightLine() {
+    currentSelectedLine.setStrokeWidth(3);
+    currentSelectedLine.setStroke(Paint.valueOf("BLACK"));
+  }
+
+  private void getPaneNode() {
     Circle c = new Circle();
+    Line l = new Line();
+
     for (Node p : pane.getChildren()) {
       try {
         p.setOnMouseClicked(
             w -> {
               if (w.getSource().getClass() == c.getClass()) {
                 unHighlightCircle(); // removes current highlight
-                a = (Circle) w.getSource(); // sets circle to a new selected circle
+                currentSelectedCircle =
+                    (Circle) w.getSource(); // sets circle to a new selected circle
                 p.toFront();
+              } else if (w.getSource().getClass() == l.getClass()) {
+                unHighglightLine();
+                currentSelectedLine = (Line) w.getSource();
+                System.out.println(currentSelectedLine);
+                p.toBack();
               } else {
                 p.toBack();
               }
@@ -157,16 +185,16 @@ public class nodeEdgeDispController {
         highlightCircle();
         startEdgeFlag = !startEdgeFlag;
         try {
-          sx = (int) a.getCenterX();
-          sy = (int) a.getCenterY();
+          sx = (int) currentSelectedCircle.getCenterX();
+          sy = (int) currentSelectedCircle.getCenterY();
         } catch (Exception exception) {
           System.out.println("no start point");
         }
       } else {
         startEdgeFlag = !startEdgeFlag;
         try {
-          ex = (int) a.getCenterX();
-          ey = (int) a.getCenterY();
+          ex = (int) currentSelectedCircle.getCenterX();
+          ey = (int) currentSelectedCircle.getCenterY();
         } catch (Exception exception) {
           System.out.println("no end point");
         }
@@ -174,6 +202,7 @@ public class nodeEdgeDispController {
         // createing the line and adding as a child to the pane
         edge ed = new edge(sx, sy, ex, ey);
         Line line = new Line(ed.startX, ed.startY, ed.endX, ed.endY);
+        line.setStrokeWidth(3);
         pane.getChildren().add(line);
         line.toBack();
         // refreshing and adding to the scene
@@ -190,6 +219,7 @@ public class nodeEdgeDispController {
     if (addNodecb.isSelected()) {
       node n = new node((int) e.getSceneX(), (int) e.getSceneY());
       Circle circle = new Circle(n.nodeX, n.nodeY, 5);
+      currentSelectedCircle = circle;
       circle.setFill(Paint.valueOf("RED"));
       pane.getChildren().add(circle);
 
@@ -212,6 +242,7 @@ public class nodeEdgeDispController {
             Integer.parseInt(endY.getText()));
     // creating a new line Node and adding it as a child to the pane
     Line line = new Line(ed.startX, ed.startY, ed.endX, ed.endY);
+    line.setStrokeWidth(3);
     pane.getChildren().add(line);
 
     // refreshing and adding to scene
@@ -225,6 +256,7 @@ public class nodeEdgeDispController {
     // to add as a child of the pane in the scene
     node n = new node(Integer.parseInt(newX.getText()), Integer.parseInt(newY.getText()));
     Circle circle = new Circle(n.nodeX, n.nodeY, 5);
+    currentSelectedCircle = circle;
     circle.setFill(Paint.valueOf("RED"));
     pane.getChildren().add(circle);
 
