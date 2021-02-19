9ce9c306a4c29460c3d5dfa31cbec4cca754e3fb
patrick-lee22 <prlee@wpi.edu>
2021-02-19 01:16:03 -0500

    Updates package name to edu.wpi.cs3733.c21.teamY

---
+++ src/main/java/edu/wpi/cs3733/c21/teamY/CustomMapController.java
@@ -0,0 +1,142 @@
+package edu.wpi.cs3733.c21.teamY;
+
+import java.util.ArrayList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class CustomMapController extends VBox {
+  @FXML private Pane basePane;
+  @FXML private Button backButton;
+  @FXML private Button clearButton;
+
+  @FXML private Canvas mapSurface;
+  @FXML private ImageView mapImage;
+  @FXML private Group mapScaleGroup;
+
+  @FXML private ComboBox startNode;
+  @FXML private ComboBox endNode;
+
+  Graph graph;
+
+  public CustomMapController() {}
+
+  private double screenRatio;
+  private double offset;
+
+  @FXML
+  public void initialize() {
+    backButton.setOnAction(e -> buttonClicked(e));
+    clearButton.setOnAction(e -> buttonClicked(e));
+
+    this.graph = ActiveGraph.getActiveGraph();
+    // mapScaleGroup.setScaleX(0.5);
+    // mapScaleGroup.setScaleY(0.5);
+
+    screenRatio = 992 / 11; // mapSurface.getWidth() / mapImage.getImage().getWidth();
+    offset = 1 * screenRatio;
+    for (Node node : graph.nodeList) {
+      startNode.getItems().add(node.nodeID);
+      endNode.getItems().add(node.nodeID);
+    }
+    redrawLabels();
+  }
+
+  public void redrawLabels() {
+    GraphicsContext g = mapSurface.getGraphicsContext2D();
+
+    g.setStroke(javafx.scene.paint.Color.color(0, 0, 0));
+
+    for (Node node : graph.nodeList) {
+
+      g.fillOval(
+          (int) (node.xcoord * screenRatio + offset / 1.7),
+          (int) (node.ycoord * screenRatio + offset),
+          5,
+          5);
+
+      g.fillText(
+          node.nodeID,
+          (int) (node.xcoord * screenRatio + offset / 1.7),
+          (int) (node.ycoord * screenRatio + offset));
+    }
+  }
+
+  @FXML
+  public void calculatePath() {
+    if (startNode.getValue() != null && endNode.getValue() != null) {
+      ArrayList<Node> nodes =
+          AStarAlgorithm.aStar(graph, (String) startNode.getValue(), (String) endNode.getValue());
+
+      System.out.println(startNode.getValue() + " " + endNode.getValue());
+
+      if (nodes != null) {
+
+        GraphicsContext g = mapSurface.getGraphicsContext2D();
+        clearMap(g);
+
+        redrawLabels();
+
+        g.setStroke(javafx.scene.paint.Color.color(0, 0, 1));
+        g.beginPath();
+        for (int i = 0; i < nodes.size() - 1; i++) {
+
+          Node node1 = nodes.get(i);
+          Node node2 = nodes.get(i + 1);
+
+          System.out.println(node1.nodeID);
+          g.setLineWidth(2.0);
+          g.moveTo(
+              (int) (node1.xcoord * screenRatio + offset / 1.7),
+              (int) (node1.ycoord * screenRatio + offset));
+          g.lineTo(
+              (int) (node2.xcoord * screenRatio + offset / 1.7),
+              (int) (node2.ycoord * screenRatio + offset));
+        }
+        g.stroke();
+      }
+    }
+  }
+
+  private void clearMap(GraphicsContext g) {
+    g.clearRect(0, 0, mapSurface.getWidth(), mapSurface.getHeight());
+  }
+
+  // button event handler
+  @FXML
+  private void buttonClicked(ActionEvent e) {
+    // error handling for FXMLLoader.load
+    try {
+      // initializing stage
+      Stage stage = null;
+
+      if (e.getSource() == backButton) {
+        // gets the current stage
+        stage = (Stage) backButton.getScene().getWindow();
+        // sets the new scene to the alex page
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("HomePage.fxml"))));
+
+      } else if (e.getSource() == clearButton) {
+        GraphicsContext g = mapSurface.getGraphicsContext2D();
+        clearMap(g);
+        redrawLabels();
+      } else {
+
+      }
+
+      // display new stage
+      stage.show();
+    } catch (Exception exp) {
+    }
+  }
+}
---
+++ src/test/java/edu/wpi/cs3733/c21/teamY/AStarAlgorithmTesting.java
@@ -0,0 +1,56 @@
+package edu.wpi.cs3733.c21.teamY;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+public class AStarAlgorithmTesting {
+  @Test
+  public void testAStarAlgorithm_nodeDistance() {
+    Node start = new Node("walk", 0.0, 0.0, "1", "Faulkner", "TestNode1", "Test", 'y', "node1");
+    Node end = new Node("walk", 3.0, 4.0, "1", "Faulkner", "TestNode2", "Test", 'y', "node2");
+    // First test that the basic functionality of nodeDistance is working
+    assertEquals(5.0, AStarAlgorithm.nodeDistance(start, end));
+    start.xcoord = 1.15;
+    start.ycoord = 1.05;
+    end.xcoord = 0.35;
+    end.ycoord = 0.45;
+    // Second test that the formula works in the negative direction as well as on decimals
+    assertEquals(1.0, AStarAlgorithm.nodeDistance(start, end));
+
+    // Third test the the formula works on a zero distance calculation
+    assertEquals(0, AStarAlgorithm.nodeDistance(start, start));
+  }
+
+  @Test
+  public void testAStarAlgorithm_aStar() {
+    ActiveGraph.initialize();
+
+    ArrayList<Node> expectPath = new ArrayList<>();
+    // Set the expected path based on the mathematical distance
+    expectPath.add(0, ActiveGraph.getActiveGraph().nodeFromID("1"));
+    expectPath.add(1, ActiveGraph.getActiveGraph().nodeFromID("5"));
+    expectPath.add(2, ActiveGraph.getActiveGraph().nodeFromID("7"));
+    expectPath.add(3, ActiveGraph.getActiveGraph().nodeFromID("6"));
+    expectPath.add(4, ActiveGraph.getActiveGraph().nodeFromID("10"));
+
+    // First test that the basic functionality of aStar is working
+    assertEquals(expectPath, AStarAlgorithm.aStar(ActiveGraph.getActiveGraph(), "1", "10"));
+
+    expectPath.clear();
+    // Set the expected path based on the mathematical distance
+    expectPath.add(0, ActiveGraph.getActiveGraph().nodeFromID("8"));
+    expectPath.add(1, ActiveGraph.getActiveGraph().nodeFromID("1"));
+    expectPath.add(2, ActiveGraph.getActiveGraph().nodeFromID("2"));
+    expectPath.add(3, ActiveGraph.getActiveGraph().nodeFromID("4"));
+
+    // Second test that the basic functionality of aStar is working on a less obvious path
+    assertEquals(expectPath, AStarAlgorithm.aStar(ActiveGraph.getActiveGraph(), "8", "4"));
+
+    expectPath.clear();
+    expectPath.add(0, ActiveGraph.getActiveGraph().nodeFromID("5"));
+    // Third test on inputting the same start and end node
+    assertEquals(expectPath, AStarAlgorithm.aStar(ActiveGraph.getActiveGraph(), "5", "5"));
+  }
+}
