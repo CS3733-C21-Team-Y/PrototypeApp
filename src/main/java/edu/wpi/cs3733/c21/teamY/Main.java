b84a2847c7d758cf28da779a3912d3b2dec94edd
BBoxell <35980906+BBoxell@users.noreply.github.com>
2021-02-20 18:33:41 -0500

    Packaged Name Updated Correct FX:IDs on the Maintenance Page

---
+++ src/main/java/edu/wpi/cs3733/c21/teamY/Main.java
@@ -0,0 +1,9 @@
+package edu.wpi.cs3733.c21.teamY;
+
+public class Main {
+
+  public static void main(String[] args) {
+    ActiveGraph.initialize();
+    App.launch(App.class, args);
+  }
+}
---
+++ src/test/java/edu/wpi/cs3733/c21/teamY/DefaultTest.java
@@ -0,0 +1,15 @@
+/*-------------------------*/
+/* DO NOT DELETE THIS TEST */
+/*-------------------------*/
+
+package edu.wpi.cs3733.c21.teamY;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class DefaultTest {
+
+  @Test
+  public void test() {}
+}
