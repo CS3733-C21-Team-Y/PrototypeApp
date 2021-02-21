17590013acf3aaebc99004fc8725d40d0c135666
chswim17 <chswim17@gmail.com>
2021-02-21 14:15:37 -0500

    added requests manager page

---
+++ src/main/java/edu/wpi/cs3733/c21/teamY/RequestsManagerController.java
@@ -0,0 +1,18 @@
+package edu.wpi.cs3733.c21.teamY;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class RequestsManagerController {
+
+    @FXML private Button backBtn;
+    @FXML private Button exitBtn;
+    @FXML private Checkbox reqCheckbox;
+
+    public RequestsManagerController(){
+
+    }
+
+
+}
