64b16cb398e306814929fc628d460322dfdea6f4
varneycole00 <61516468+varneycole00@users.noreply.github.com>
2021-02-28 17:45:52 -0500

    Addition of email and password fields in employee table, class, as well as functionality needed.

--- src/main/java/edu/wpi/cs3733/c21/teamY/pages/ServiceRequestManagerSubpageController.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/ServiceRequestManagerSubpageController.java
@@ -1,58 +1,41 @@
 package edu.wpi.cs3733.c21.teamY.pages;
 
 import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javax.swing.*;
 
-public class ServiceRequestManagerSubpageController extends RightPage {
+public class ServiceRequestManagerSubpageController {
   @FXML private JFXButton laundryBtn;
   @FXML private JFXButton maintenanceBtn;
-  @FXML private JFXButton AVBtn;
-  @FXML private JFXButton floralBtn;
-  @FXML private JFXButton languageBtn;
-  @FXML private JFXButton giftBtn;
-  @FXML private JFXButton medicineBtn;
-  @FXML private JFXButton securityBtn;
-  @FXML private JFXButton insideHosBtn;
-  @FXML private JFXButton computerBtn;
-  @FXML private JFXButton outsideHosBtn;
-  @FXML private JFXButton sanitizationBtn;
 
   @FXML
   private void initialize() {
     laundryBtn.setOnAction(e -> pageButtonClicked(e));
-    maintenanceBtn.setOnAction(e -> pageButtonClicked(e));
-    AVBtn.setOnAction(e -> pageButtonClicked(e));
-    floralBtn.setOnAction(e -> pageButtonClicked(e));
-    languageBtn.setOnAction(e -> pageButtonClicked(e));
-    giftBtn.setOnAction(e -> pageButtonClicked(e));
-    medicineBtn.setOnAction(e -> pageButtonClicked(e));
-    securityBtn.setOnAction(e -> pageButtonClicked(e));
-    insideHosBtn.setOnAction(e -> pageButtonClicked(e));
-    computerBtn.setOnAction(e -> pageButtonClicked(e));
-    outsideHosBtn.setOnAction(e -> pageButtonClicked(e));
-    sanitizationBtn.setOnAction(e -> pageButtonClicked(e));
-  }
-
-  @Override
-  public void loadNavigationBar() {
-    parent.setCenterColumnWidth(350);
   }
 
   @FXML
   private void pageButtonClicked(ActionEvent e) {
+    Stage stage = (Stage) ((JFXButton) e.getSource()).getScene().getWindow();
+    if (e.getSource() == laundryBtn)
+      stage.setUserData(
+          new StageInformation(
+              ((StageInformation) stage.getUserData()).getCenterPaneFXML(), "LaundrySubPage.fxml"));
+    else if (e.getSource() == maintenanceBtn)
+      stage.setUserData(
+          new StageInformation(
+              ((StageInformation) stage.getUserData()).getCenterPaneFXML(),
+              "MaintenanceSubPage.fxml"));
 
-    if (e.getSource() == laundryBtn) parent.loadRightSubPage("LaundrySubPage.fxml");
-    else if (e.getSource() == maintenanceBtn) parent.loadRightSubPage("MaintenanceSubPage.fxml");
-    else if (e.getSource() == AVBtn) parent.loadRightSubPage("AudioVisualSubPage.fxml");
-    else if (e.getSource() == floralBtn) parent.loadRightSubPage("FloralDeliverySubPage.fxml");
-    else if (e.getSource() == languageBtn) parent.loadRightSubPage("LanguageSubPage.fxml");
-    else if (e.getSource() == giftBtn) parent.loadRightSubPage("GiftDeliverySubPage.fxml");
-    else if (e.getSource() == medicineBtn) parent.loadRightSubPage("MedicineSubPage.fxml");
-    else if (e.getSource() == securityBtn) parent.loadRightSubPage("SecuritySubPage.fxml");
-    else if (e.getSource() == insideHosBtn) parent.loadRightSubPage("InsideHospitalSubPage.fxml");
-    else if (e.getSource() == computerBtn) parent.loadRightSubPage("ITSubPage.fxml");
-    else if (e.getSource() == outsideHosBtn) parent.loadRightSubPage("OutsideHospitalSubPage.fxml");
-    else if (e.getSource() == sanitizationBtn) parent.loadRightSubPage("SanitizationSubPage.fxml");
+    try {
+      stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("MaintenancePage.fxml"))));
+    } catch (IOException ioException) {
+      ioException.printStackTrace();
+    }
+    stage.show();
   }
 }
