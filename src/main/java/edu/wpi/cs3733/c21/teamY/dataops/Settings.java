06c23e072c1dfdf64edfc7f4ac4273ed14fe617b
patrick-lee22 <prlee@wpi.edu>
2021-02-28 16:59:20 -0500

    Adds singleton settings class

---
+++ src/main/java/edu/wpi/cs3733/c21/teamY/dataops/Settings.java
@@ -0,0 +1,28 @@
+package edu.wpi.cs3733.c21.teamY.dataops;
+
+public class Settings {
+
+    private static Settings settings = new Settings();
+    private String currentUsername;
+    private String currentPermissions;
+
+    private Settings(){}
+
+    public String getCurrentUsername() {
+        return currentUsername;
+    }
+
+    public String getCurrentPermissions() {
+        return currentPermissions;
+    }
+
+    public void loginSuccess(String username, String permissions) {
+        this.currentUsername = username;
+        this.currentPermissions = permissions;
+    }
+
+    public void logout() {
+        this.currentUsername = null;
+        this.currentPermissions = null;
+    }
+}
