32d810812d51edcf91a5673872349bd7b83873cb
BBoxell <35980906+BBoxell@users.noreply.github.com>
2021-02-28 17:06:55 -0500

    Solved FontAwesome issues and began work on page transitions

---
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/ServiceRequestManagerSubpageController.java
@@ -0,0 +1,42 @@
+package edu.wpi.cs3733.c21.teamY.pages;
+
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.cs3733.c21.teamY.dataops.JDBCUtils;
+import edu.wpi.cs3733.c21.teamY.entity.Service;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class ServiceRequestManagerSubpageController {
+    @FXML
+    private JFXButton laundryBtn;
+    @FXML private JFXButton maintenanceBtn;
+
+    @FXML
+    private void initialize() {
+        laundryBtn.setOnAction(e -> pageButtonClicked(e));
+    }
+
+    @FXML
+    private void pageButtonClicked(ActionEvent e) {
+        Stage stage = (Stage) ((JFXButton) e.getSource()).getScene().getWindow();
+        if (e.getSource() == laundryBtn)
+            stage.setUserData(new StageInformation(((StageInformation) stage.getUserData()).getCenterPaneFXML(), "LaundrySubPage.fxml"));
+        else if (e.getSource() == maintenanceBtn)
+            stage.setUserData(new StageInformation(((StageInformation) stage.getUserData()).getCenterPaneFXML(), "MaintenanceSubPage.fxml"));
+
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("MaintenancePage.fxml"))));
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        stage.show();
+    }
+}
+
---
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/StageInformation.java
@@ -0,0 +1,29 @@
+package edu.wpi.cs3733.c21.teamY.pages;
+
+public class StageInformation {
+
+    private String centerPaneFXML;
+    private String rightPaneFXML;
+
+    public StageInformation(String centerPaneFXML, String rightPaneFXML) {
+        this.centerPaneFXML = centerPaneFXML;
+        this.rightPaneFXML = rightPaneFXML;
+    }
+
+    public String getCenterPaneFXML() {
+        return centerPaneFXML;
+    }
+
+    public void setCenterPaneFXML(String centerPaneFXML) {
+        this.centerPaneFXML = centerPaneFXML;
+    }
+
+    public String getRightPaneFXML() {
+        return rightPaneFXML;
+    }
+
+    public void setRightPaneFXML(String rightPaneFXML) {
+        this.rightPaneFXML = rightPaneFXML;
+    }
+
+}
