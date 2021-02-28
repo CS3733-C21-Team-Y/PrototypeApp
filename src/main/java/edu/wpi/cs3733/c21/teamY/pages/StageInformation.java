d809762a2d83c271c38f0dc2af0c7ec638d44047
BBoxell <35980906+BBoxell@users.noreply.github.com>
2021-02-28 17:12:43 -0500

    It runs now. Not correctly, but it does

--- src/main/java/edu/wpi/cs3733/c21/teamY/pages/StageInformation.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/StageInformation.java
@@ -2,28 +2,27 @@ package edu.wpi.cs3733.c21.teamY.pages;
 
 public class StageInformation {
 
-    private String centerPaneFXML;
-    private String rightPaneFXML;
+  private String centerPaneFXML;
+  private String rightPaneFXML;
 
-    public StageInformation(String centerPaneFXML, String rightPaneFXML) {
-        this.centerPaneFXML = centerPaneFXML;
-        this.rightPaneFXML = rightPaneFXML;
-    }
+  public StageInformation(String centerPaneFXML, String rightPaneFXML) {
+    this.centerPaneFXML = centerPaneFXML;
+    this.rightPaneFXML = rightPaneFXML;
+  }
 
-    public String getCenterPaneFXML() {
-        return centerPaneFXML;
-    }
+  public String getCenterPaneFXML() {
+    return centerPaneFXML;
+  }
 
-    public void setCenterPaneFXML(String centerPaneFXML) {
-        this.centerPaneFXML = centerPaneFXML;
-    }
+  public void setCenterPaneFXML(String centerPaneFXML) {
+    this.centerPaneFXML = centerPaneFXML;
+  }
 
-    public String getRightPaneFXML() {
-        return rightPaneFXML;
-    }
+  public String getRightPaneFXML() {
+    return rightPaneFXML;
+  }
 
-    public void setRightPaneFXML(String rightPaneFXML) {
-        this.rightPaneFXML = rightPaneFXML;
-    }
-
+  public void setRightPaneFXML(String rightPaneFXML) {
+    this.rightPaneFXML = rightPaneFXML;
+  }
 }
