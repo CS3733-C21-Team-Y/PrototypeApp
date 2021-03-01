224229ba29f3adfe1b887cc227cd464ef16b0f5a
patrick-lee22 <prlee@wpi.edu>
2021-03-01 03:24:24 -0500

    Floral Delivery works :>

--- src/main/java/edu/wpi/cs3733/c21/teamY/pages/FloralDeliverySubPageController.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/FloralDeliverySubPageController.java
@@ -1,30 +1,34 @@
 package edu.wpi.cs3733.c21.teamY.pages;
 
-import com.jfoenix.controls.JFXButton;
-import edu.wpi.cs3733.c21.teamY.dataops.JDBCUtils;
+import com.jfoenix.controls.*;
+import edu.wpi.cs3733.c21.teamY.dataops.DataOperations;
+import edu.wpi.cs3733.c21.teamY.dataops.Settings;
 import edu.wpi.cs3733.c21.teamY.entity.Service;
-import java.io.IOException;
 import java.sql.SQLException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class FloralDeliverySubPageController extends GenericServiceFormPage {
 
   @FXML private JFXButton clearBtn;
   @FXML private JFXButton backBtn;
   @FXML private JFXButton submitBtn;
+  @FXML private JFXTextField roomNumberInput;
+  @FXML private JFXTextField categoryInput;
+  @FXML private JFXTextField fromInput;
+  @FXML private JFXTextField toInput;
+  @FXML private JFXTextArea descriptionInput;
+  @FXML private JFXTextField dateInput;
+  Settings settings;
 
   public FloralDeliverySubPageController() {}
 
   // this runs once the FXML loads in to attach functions to components
   @FXML
   private void initialize() {
-
+    settings = Settings.getSettings();
     backBtn.setOnAction(e -> buttonClicked(e));
-    // submitBtn.setOnAction(e -> submitBtnClicked());
+    submitBtn.setOnAction(e -> submitBtnClicked());
   }
 
   private void buttonClicked(ActionEvent e) {
@@ -34,19 +38,19 @@ public class FloralDeliverySubPageController extends GenericServiceFormPage {
   @FXML
   private void submitBtnClicked() {
     // put code for submitting a service request here
-    Stage stage = null;
     Service service = new Service(this.IDCount, "Floral Delivery");
+    this.IDCount++;
+    service.setLocation(roomNumberInput.getText());
+    service.setCategory(categoryInput.getText());
+    service.setDescription(descriptionInput.getText());
+    service.setRequester(settings.getCurrentUsername());
+    service.setAdditionalInfo(fromInput.getText() + " " + toInput.getText());
+    service.setDate(dateInput.getText());
     try {
-      stage = (Stage) submitBtn.getScene().getWindow();
-      JDBCUtils.saveService(service);
-      stage.setScene(
-          new Scene(FXMLLoader.load(getClass().getResource("FloralDeliverySubPage.fxml"))));
-      stage.show();
+      DataOperations.saveService(service);
     } catch (SQLException throwables) {
       throwables.printStackTrace();
     } catch (IllegalAccessException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
       e.printStackTrace();
     }
   }
