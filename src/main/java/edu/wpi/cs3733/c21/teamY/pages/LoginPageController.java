b0c8be39514dcb52e1e9c3700cbd18f058c240ae
patrick-lee22 <prlee@wpi.edu>
2021-03-01 00:39:47 -0500

    Fixes and Dialog box for bad login info

--- src/main/java/edu/wpi/cs3733/c21/teamY/pages/LoginPageController.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/LoginPageController.java
@@ -1,18 +1,22 @@
 package edu.wpi.cs3733.c21.teamY.pages;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.c21.teamY.dataops.DataOperations;
-import javafx.fxml.FXML;
-
+import java.sql.SQLException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 
 public class LoginPageController extends RightPage {
   @FXML JFXTextField employeeIDTextField;
   @FXML JFXPasswordField passwordTextField;
   @FXML JFXButton loginBtn;
   @FXML JFXButton guestBtn;
+  @FXML StackPane stackPane;
 
   @FXML
   private void initialize() {
@@ -20,11 +24,22 @@ public class LoginPageController extends RightPage {
   }
 
   private void buttonClicked(ActionEvent e) {
-      if(e.getSource() == loginBtn) {
-        String tryID = employeeIDTextField.getText();
-        String tryPwd = passwordTextField.getText();
+    if (e.getSource() == loginBtn) {
+      String tryID = employeeIDTextField.getText();
+      String tryPwd = passwordTextField.getText();
 
-        //login???
+      try {
+        if (DataOperations.findUser(tryID, tryPwd)) {
+          parent.loadRightSubPage("ServiceRequestManagerSubpage.fxml");
+        } else {
+          JFXDialog errorMsg = new JFXDialog();
+          errorMsg.setContent(
+              new Label("Username or password not recognized" + "\n please try again"));
+          errorMsg.show(stackPane);
+        }
+      } catch (SQLException throwables) {
+        throwables.printStackTrace();
       }
+    }
   }
 }
