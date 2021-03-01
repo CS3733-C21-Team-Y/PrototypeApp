d8d8a8c9d6929f3bf1fff3535dc6db118e919a98
sambraun <78232058+sambraun@users.noreply.github.com>
2021-03-01 00:22:27 -0500

    all back buttons work yay

--- src/main/java/edu/wpi/cs3733/c21/teamY/pages/FloralDeliverySubPageController.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/FloralDeliverySubPageController.java
@@ -1,3 +1,53 @@
 package edu.wpi.cs3733.c21.teamY.pages;
 
-public class FloralDeliverySubPageController extends GenericServiceFormPage {}
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.cs3733.c21.teamY.dataops.JDBCUtils;
+import edu.wpi.cs3733.c21.teamY.entity.Service;
+import java.io.IOException;
+import java.sql.SQLException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FloralDeliverySubPageController extends GenericServiceFormPage {
+
+  @FXML private JFXButton clearBtn;
+  @FXML private JFXButton backBtn;
+  @FXML private JFXButton submitBtn;
+
+  public FloralDeliverySubPageController() {}
+
+  // this runs once the FXML loads in to attach functions to components
+  @FXML
+  private void initialize() {
+
+    backBtn.setOnAction(e -> buttonClicked(e));
+    // submitBtn.setOnAction(e -> submitBtnClicked());
+  }
+
+  private void buttonClicked(ActionEvent e) {
+    if (e.getSource() == backBtn) parent.loadRightSubPage("ServiceRequestManagerSubpage.fxml");
+  }
+
+  @FXML
+  private void submitBtnClicked() {
+    // put code for submitting a service request here
+    Stage stage = null;
+    Service service = new Service(this.IDCount, "Floral Delivery");
+    try {
+      stage = (Stage) submitBtn.getScene().getWindow();
+      JDBCUtils.saveService(service);
+      stage.setScene(
+          new Scene(FXMLLoader.load(getClass().getResource("FloralDeliverySubPage.fxml"))));
+      stage.show();
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
--- src/main/java/edu/wpi/cs3733/c21/teamY/pages/MedicineSubPageController.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/MedicineSubPageController.java
@@ -1,3 +1,25 @@
 package edu.wpi.cs3733.c21.teamY.pages;
 
-public class MedicineSubPageController extends GenericServiceFormPage {}
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MedicineSubPageController extends GenericServiceFormPage {
+
+  @FXML private JFXButton clearBtn;
+  @FXML private JFXButton backBtn;
+  @FXML private JFXButton submitBtn;
+
+  public MedicineSubPageController() {}
+
+  @FXML
+  private void initialize() {
+
+    backBtn.setOnAction(e -> buttonClicked(e));
+    // submitBtn.setOnAction(e -> submitBtnClicked());
+  }
+
+  private void buttonClicked(ActionEvent e) {
+    if (e.getSource() == backBtn) parent.loadRightSubPage("ServiceRequestManagerSubpage.fxml");
+  }
+}
