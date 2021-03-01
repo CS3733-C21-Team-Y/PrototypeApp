6590546aa6d4816dbeeb846c7dfab8c45a14c7b1
BBoxell <35980906+BBoxell@users.noreply.github.com>
2021-02-28 21:36:05 -0500

    Added a bunch of FXML files

--- src/main/java/edu/wpi/cs3733/c21/teamY/pages/ServiceRequestManagerSubpageController.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/ServiceRequestManagerSubpageController.java
@@ -8,12 +8,14 @@ public class ServiceRequestManagerSubpageController extends RightPage {
   @FXML private JFXButton laundryBtn;
   @FXML private JFXButton maintenanceBtn;
   @FXML private JFXButton AVBtn;
+  @FXML private JFXButton floralBtn;
 
   @FXML
   private void initialize() {
     laundryBtn.setOnAction(e -> pageButtonClicked(e));
     maintenanceBtn.setOnAction(e -> pageButtonClicked(e));
     AVBtn.setOnAction(e -> pageButtonClicked(e));
+    floralBtn.setOnAction(e -> pageButtonClicked(e));
   }
 
   @FXML
@@ -22,5 +24,6 @@ public class ServiceRequestManagerSubpageController extends RightPage {
     if (e.getSource() == laundryBtn) parent.loadRightSubPage("LaundrySubPage.fxml");
     else if (e.getSource() == maintenanceBtn) parent.loadRightSubPage("MaintenanceSubPage.fxml");
     else if (e.getSource() == AVBtn) parent.loadRightSubPage("AudioVisualSubPage.fxml");
+    else if (e.getSource() == floralBtn) parent.loadRightSubPage("FloralDeliverySubPage.fxml");
   }
 }
