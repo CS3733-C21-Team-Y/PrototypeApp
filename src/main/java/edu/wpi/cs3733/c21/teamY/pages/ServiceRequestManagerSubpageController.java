4368f76ffedcdebd539f45423f14056035bc3972
sambraun <78232058+sambraun@users.noreply.github.com>
2021-02-28 22:11:37 -0500

    Connected most individual sub pages

--- src/main/java/edu/wpi/cs3733/c21/teamY/pages/ServiceRequestManagerSubpageController.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/ServiceRequestManagerSubpageController.java
@@ -9,6 +9,11 @@ public class ServiceRequestManagerSubpageController extends RightPage {
   @FXML private JFXButton maintenanceBtn;
   @FXML private JFXButton AVBtn;
   @FXML private JFXButton floralBtn;
+  @FXML private JFXButton languageBtn;
+  @FXML private JFXButton giftBtn;
+  @FXML private JFXButton medicineBtn;
+  @FXML private JFXButton securityBtn;
+  @FXML private JFXButton insideHosBtn;
 
   @FXML
   private void initialize() {
@@ -16,6 +21,11 @@ public class ServiceRequestManagerSubpageController extends RightPage {
     maintenanceBtn.setOnAction(e -> pageButtonClicked(e));
     AVBtn.setOnAction(e -> pageButtonClicked(e));
     floralBtn.setOnAction(e -> pageButtonClicked(e));
+    languageBtn.setOnAction(e -> pageButtonClicked(e));
+    giftBtn.setOnAction(e -> pageButtonClicked(e));
+    medicineBtn.setOnAction(e -> pageButtonClicked(e));
+    securityBtn.setOnAction(e -> pageButtonClicked(e));
+    insideHosBtn.setOnAction(e -> pageButtonClicked(e));
   }
 
   @FXML
@@ -25,5 +35,10 @@ public class ServiceRequestManagerSubpageController extends RightPage {
     else if (e.getSource() == maintenanceBtn) parent.loadRightSubPage("MaintenanceSubPage.fxml");
     else if (e.getSource() == AVBtn) parent.loadRightSubPage("AudioVisualSubPage.fxml");
     else if (e.getSource() == floralBtn) parent.loadRightSubPage("FloralDeliverySubPage.fxml");
+    else if (e.getSource() == languageBtn) parent.loadRightSubPage("LanguageSubPage.fxml");
+    else if (e.getSource() == giftBtn) parent.loadRightSubPage("GiftDeliverySubPage.fxml");
+    else if (e.getSource() == medicineBtn) parent.loadRightSubPage("MedicineSubPage.fxml");
+    else if (e.getSource() == securityBtn) parent.loadRightSubPage("SecuritySubPage.fxml");
+    else if (e.getSource() == insideHosBtn) parent.loadRightSubPage("InsideHospitalSubPage.fxml");
   }
 }
