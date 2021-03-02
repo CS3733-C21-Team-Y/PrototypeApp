a71f175169b2b255a7701e88b3f4890d4ca7dd52
chswim17 <chswim17@gmail.com>
2021-03-02 00:59:36 -0500

    covid page no work

--- src/main/java/edu/wpi/cs3733/c21/teamY/pages/CovidScreeningController.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/CovidScreeningController.java
@@ -3,9 +3,8 @@ package edu.wpi.cs3733.c21.teamY.pages;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import javafx.fxml.FXML;
-import javafx.stage.Stage;
 
-public class CovidScreeningController extends RightPage{
+public class CovidScreeningController extends RightPage {
 
   @FXML private JFXCheckBox posY;
   @FXML private JFXCheckBox posN;
@@ -15,27 +14,37 @@ public class CovidScreeningController extends RightPage{
   @FXML private JFXCheckBox closeN;
   @FXML private JFXButton submitBtn;
 
+  //  private Settings settings;
+
   public CovidScreeningController() {}
 
+  @FXML
   private void initialize() {
+    //    settings = Settings.getSettings();
+
     submitBtn.setOnAction(e -> submitBtnClicked());
-    posY.setSelected(false);
-    posN.setSelected(false);
-    sympY.setSelected(false);
-    sympN.setSelected(false);
-    closeY.setSelected(false);
-    closeN.setSelected(false);
+//    posY.setSelected(false);
+//    posN.setSelected(false);
+//    sympY.setSelected(false);
+//    sympN.setSelected(false);
+//    closeY.setSelected(false);
+//    closeN.setSelected(false);
   }
 
   @FXML
   private void submitBtnClicked() {
+
+    boolean isPositive = (posN.isSelected() && !posY.isSelected());
+    boolean hasSymp = (sympN.isSelected() && !sympY.isSelected());
     // check y/n status on each checkbox
-        if (posN.isSelected() && sympN.isSelected() && closeN.isSelected()){
-
-        } else {
-          // give instructions on how to go to covid entrance point
-        }
+    if (!isPositive && !hasSymp) {
+      parent.loadRightSubPage("LoginPage.fxml");
+    } else {
+      // give instructions on how to go to covid entrance point
+    }
     //
-
   }
+
+  @FXML
+  private void checkBoxClicked() {}
 }
