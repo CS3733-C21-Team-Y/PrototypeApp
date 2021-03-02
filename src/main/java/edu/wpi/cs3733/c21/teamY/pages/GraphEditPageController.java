fa2af29bc7789fcca87288e755d626ff4734a790
BBoxell <35980906+BBoxell@users.noreply.github.com>
2021-03-01 23:44:23 -0500

    Nav Page UI looks better

--- src/main/java/edu/wpi/cs3733/c21/teamY/pages/GraphEditPageController.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/GraphEditPageController.java
@@ -230,7 +230,7 @@ public class GraphEditPageController {
     toolTip.toFront();
 
     initImage();
-    mapInsertController.getFloorMenu().setText("Parking Lot");
+    mapInsertController.getFloorMenu().setPromptText("Parking Lot");
 
     loadNodesButton.setOnAction(
         e -> {
@@ -238,16 +238,16 @@ public class GraphEditPageController {
         });
 
     int i = 0;
-    for (MenuItem menuItem : mapInsertController.getFloorMenu().getItems()) {
-      int index = i;
-      menuItem.setOnAction(
-          e -> {
-            mapInsertController.removeAllAdornerElements();
-            mapInsertController.changeMapImage(mapInsertController.getMapOrder().get(index));
-            mapInsertController.updateMenuPreview(e, mapInsertController.getFloorMenu());
-          });
-      i++;
-    }
+    //    for (MenuItem menuItem : mapInsertController.getFloorMenu().getItems()) {
+    //      int index = i;
+    //      menuItem.setOnAction(
+    //          e -> {
+    //            mapInsertController.removeAllAdornerElements();
+    //            mapInsertController.changeMapImage(mapInsertController.getMapOrder().get(index));
+    //            mapInsertController.updateMenuPreview(e, mapInsertController.getFloorMenu());
+    //          });
+    //      i++;
+    //    }
 
     // attaches a handler to the button with a lambda expression
     toHomeBtn.setOnAction(e -> buttonClicked(e));
