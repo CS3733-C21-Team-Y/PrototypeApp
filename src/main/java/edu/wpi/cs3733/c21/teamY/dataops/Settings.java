ac395c3c467359591d7164fb8c393b8855b34881
BBoxell <35980906+BBoxell@users.noreply.github.com>
2021-03-05 23:05:22 -0500

    Made mobilemain fxml page

--- src/main/java/edu/wpi/cs3733/c21/teamY/dataops/Settings.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/dataops/Settings.java
@@ -2,34 +2,34 @@ package edu.wpi.cs3733.c21.teamY.dataops;
 
 public class Settings {
 
-  private static Settings settings = new Settings();
-  private String currentUsername;
-  private int currentPermissions;
+    private static Settings settings = new Settings();
+    private String currentUsername;
+    private int currentPermissions;
 
-  private Settings() {
-    this.currentUsername = "guest";
-    this.currentPermissions = 0;
-  }
+    private Settings() {
+        this.currentUsername = "guest";
+        this.currentPermissions = 0;
+    }
 
-  public String getCurrentUsername() {
-    return currentUsername;
-  }
+    public String getCurrentUsername() {
+        return currentUsername;
+    }
 
-  public int getCurrentPermissions() {
-    return currentPermissions;
-  }
+    public int getCurrentPermissions() {
+        return currentPermissions;
+    }
 
-  public static Settings getSettings() {
-    return settings;
-  }
+    public static Settings getSettings() {
+        return settings;
+    }
 
-  public void loginSuccess(String username, int permissions) {
-    this.currentUsername = username;
-    this.currentPermissions = permissions;
-  }
+    public void loginSuccess(String username, int permissions) {
+        this.currentUsername = username;
+        this.currentPermissions = permissions;
+    }
 
-  public void logout() {
-    this.currentUsername = null;
-    this.currentPermissions = -1;
-  }
+    public void logout() {
+        this.currentUsername = null;
+        this.currentPermissions = -1;
+    }
 }
