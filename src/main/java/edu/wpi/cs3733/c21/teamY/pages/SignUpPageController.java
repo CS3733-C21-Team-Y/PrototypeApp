94aa40bc29266f3747393afc15b97fa1fb421c19
Yonghua Wang <ywang33@wpi.edu>
2021-03-07 11:57:30 -0500

    about page FXML

---
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/SignUpPageController.java
@@ -0,0 +1,16 @@
+package edu.wpi.cs3733.c21.teamY.pages;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SignUpPageController {
+  @FXML private Button clearBtn;
+  @FXML private Button signupBtn;
+  @FXML private Button exitBtn;
+  @FXML private JFXTextField usernameTextField;
+  @FXML private JFXTextField passwordTextField;
+  @FXML private JFXTextField firstnameTextField;
+  @FXML private JFXTextField lastnameTextField;
+  @FXML private JFXTextField emailTextField;
+}
