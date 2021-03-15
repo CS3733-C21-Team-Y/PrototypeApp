73a694925c3dbf278a74c13225e9a1de4f174979
IanBeazley <raymondibeazley@gmail.com>
2021-03-15 00:26:08 -0400

    Robot connected to service requests?

--- src/main/java/edu/wpi/cs3733/c21/teamY/pages/AdminPageController.java
+++ src/main/java/edu/wpi/cs3733/c21/teamY/pages/AdminPageController.java
@@ -12,7 +12,10 @@ import edu.wpi.cs3733.c21.teamY.dataops.Settings;
 import edu.wpi.cs3733.c21.teamY.entity.ActiveGraph;
 import edu.wpi.cs3733.c21.teamY.entity.Edge;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import edu.wpi.cs3733.c21.teamY.entity.Service;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
@@ -131,8 +134,21 @@ public class AdminPageController extends SubPage {
           */
 
           ArrayList<String> end = new ArrayList<>();
+          ArrayList<Service> services = new ArrayList<>();
+
+          try {
+            services = DataOperations.exportService("Laundry","");
+          } catch (SQLException throwables) {
+            throwables.printStackTrace();
+          }
+          for (Service service : services) {
+            end.add(ActiveGraph.getActiveGraph().longNodes.get(service.getLocation()).nodeID);
+          }
+          /* Brute force approach
           end.add("PDEPT00601");
           end.add("PDEPT00201");
+           */
+          //This adds our starting location as the last point we go to
           end.add("PSERV00301");
           x = AlgorithmCalls.aStar(ActiveGraph.getActiveGraph(), "PSERV00301", end, "");
 
